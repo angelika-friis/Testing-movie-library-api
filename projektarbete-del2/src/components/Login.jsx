35054bfdb0bd2a53350eadc158e3da711fb58017
hampusvh <hampus.vh@gmail.com>
2025-04-22 12:34:04 +0200

    login & jwt done

--- projektarbete-del2/src/components/Login.jsx
+++ projektarbete-del2/src/components/Login.jsx
@@ -2,6 +2,26 @@ import { useState } from "react";
 
 function Login() {
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [token, setToken] = useState("");
+
+  const handleLogin = async () => {
+    const res = await fetch(
+      "https://tokenservice-jwt-2025.fly.dev/token-service/v1/request-token",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      }
+    );
+    const jwt = await res.text();
+    setToken(jwt);
+  };
+
   return (
     <div>
       <h2>Login</h2>
@@ -11,6 +31,13 @@ function Login() {
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
+      <input
+        type="password"
+        placeholder="Lösenord"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Logga in</button>
     </div>
   );
 }
